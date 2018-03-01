b3f20ca3b77ec92863ed0427deae79b26471be5b
Jose Maria Lopez <jmlopez@tuenti.com>
2018-03-01 16:15:05 +0100

    Added random victory sound in league when a player wins all the rounds on a match

--- plugins/sound.py
+++ plugins/sound.py
@@ -14,7 +14,8 @@ class Plugin:
         'reset': ['whistle_2short1long'],
         'competition': ['Air Horn-SoundBible.com-964603082-lower'],
         'sudden_death': ['buzzer2'],
-        'timeout_close': ['Woop_Woop-SoundBible.com-198943467']
+        'timeout_close': ['Woop_Woop-SoundBible.com-198943467'],
+        'winner_random_sound': ['street_fighter-you_win', 'dragon_ball-kame_hame_ha']
     }
 
     def __init__(self, bus):
@@ -64,8 +65,10 @@ class Plugin:
             sounds.append('sudden_death')
         elif ev.name == 'timeout_close':
             sounds.append('timeout_close')
-        elif ev.name == 'start_competition' or ev.name == 'end_competition':
+        elif ev.name == 'start_competition':
             sounds.append('competition')
+        elif ev.name == 'end_competition':
+            sounds.append(self.get_end_competition_sound(ev.data['points']))
         else:
             return
 
@@ -73,6 +76,29 @@ class Plugin:
 
         for s in sounds:
             self.play(s)
+
+    def get_end_competition_sound(self, points):
+        unique_winner = self.get_unique_winner(points)
+        if unique_winner:
+            time.sleep(2)
+            return self.get_winner_custom_sound(unique_winner)
+        else:
+            return 'competition';
+
+    def get_unique_winner(self, points):
+        max_score = 0
+        player = None
+        for name, ps in points.items():
+            if ps > max_score:
+                max_score = ps
+                player = name
+            elif ps == max_score:
+                player = None
+        return player
+
+    def get_winner_custom_sound(self, winner):
+        return 'winner_random_sound';
+
 
 
 if __name__ == "__main__":
