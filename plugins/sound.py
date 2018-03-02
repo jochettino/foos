26b4751d2bf1bd4395b1808ddad5b3432d4e77ae
Jose Maria Lopez <jmlopez@tuenti.com>
2018-03-02 11:45:46 +0100

    Added winner custom sounds read from config file. Added new ogg winner sounds

--- plugins/sound.py
+++ plugins/sound.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import foos.config as config
 import time
 import subprocess
 import random
 import os
+import logging
+import json
+
+logger = logging.getLogger(__name__)
 
 
 class Plugin:
@@ -14,8 +19,7 @@ class Plugin:
         'reset': ['whistle_2short1long'],
         'competition': ['Air Horn-SoundBible.com-964603082-lower'],
         'sudden_death': ['buzzer2'],
-        'timeout_close': ['Woop_Woop-SoundBible.com-198943467'],
-        'winner_random_sound': ['street_fighter-you_win', 'dragon_ball-kame_hame_ha']
+        'timeout_close': ['Woop_Woop-SoundBible.com-198943467']
     }
 
     def __init__(self, bus):
@@ -72,7 +76,11 @@ class Plugin:
         else:
             return
 
-        sounds = [self.sounds_dir + "/{}.wav".format(self.choose_sound(sound)) for sound in sounds]
+        if (ev.name == 'end_competition' and sounds.count('competition') == 0):
+            # get winner custom sound path
+            sounds = [self.get_custom_sound_key_path(sound) for sound in sounds]
+        else:
+            sounds = [self.sounds_dir + "/{}.wav".format(self.choose_sound(sound)) for sound in sounds]
 
         for s in sounds:
             self.play(s)
@@ -80,8 +88,9 @@ class Plugin:
     def get_end_competition_sound(self, points):
         unique_winner = self.get_unique_winner(points)
         if unique_winner:
+            logger.info("There is an unique winner: %s", unique_winner)
             time.sleep(2)
-            return self.get_winner_custom_sound(unique_winner)
+            return self.get_custom_sound_from_config(unique_winner)
         else:
             return 'competition';
 
@@ -96,9 +105,27 @@ class Plugin:
                 player = None
         return player
 
-    def get_winner_custom_sound(self, winner):
-        return 'winner_random_sound';
+    def get_custom_sound_from_config(self, winner):
+        sound_key = None
+        if hasattr(config, 'winner_sound_file'):
+            winner_sound_file = config.winner_sound_file
+            if os.path.exists(winner_sound_file):
+                logger.info("Reading '%s' winner custom sound file", winner_sound_file)
+                with open(winner_sound_file) as f:
+                    winner_sound_dict = json.load(f)
+                    sound_key = winner_sound_dict.get(winner)
+                    if sound_key == None:
+                        logger.info("Not found custom sound for %s", winner)
+                    else:
+                        logger.info("Found custom sound: %s -> %s", winner, sound_key)
+            else:
+                logger.warn("Not found winner sound file %s", winner_sound_file)
+        
+        return sound_key or 'competition'
 
+    def get_custom_sound_key_path(self, custom_sound_key):
+        ## TODO Add more conditions for example if its http url, youtube, etc
+        return self.sounds_dir + "/" + custom_sound_key
 
 
 if __name__ == "__main__":
